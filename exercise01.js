48d613fcf03fff78b5cba6703a65254c41735a92
GabrielBetini <contatobetini@outlook.com>
2023-11-16 21:40:59 -0300

    1-2

---
+++ exercise01.js
@@ -0,0 +1,80 @@
+/**Crie uma função que receba como parâmetro a lista de hotifrutis abaixo que tem três tipos: `frutas`, `legumes` e `verduras`, faça com que essa função retorne o número de itens de cada um desses tipos.
+
+Sendo que:
+
+fruta → banana, maçã, abacaxi, melancia, laranja, limão.
+
+legume → cenoura, batata, brócolis, abóbora, tomate, alho-poró.
+
+verdura → alface, espinafre, rúcula, couve, agrião, acelga. */
+
+console.log('---------- HORTIFRUTI ----------');
+
+const hortifrutis = [
+	'banana',
+	'agrião',
+	'maçã',
+	'abacaxi',
+	'melancia',
+	'laranja',
+	'limão',
+	'cenoura',
+	'melancia',
+	'batata',
+	'brócolis',
+	'abóbora',
+	'tomate',
+	'alho-poró',
+	'alface',
+	'espinafre',
+	'rúcula',
+	'couve',
+	'acelga',
+	'maçã',
+	'batata',
+];
+
+const frutas = [];
+const legumes = [];
+const verduras = [];
+
+hortifrutis.forEach(Element => {
+	switch (Element) {
+		case 'banana':
+		case 'maçã':
+		case 'abacaxi':
+		case 'melancia':
+		case 'laranja':
+		case 'limão':
+			frutas.push(Element);
+			break;
+		case 'cenoura':
+		case 'batata':
+		case 'brócolis':
+		case 'abóbora':
+		case 'tomate':
+		case 'alho-poró':
+			legumes.push(Element);
+			break;
+		case 'alface':
+		case 'espinafre':
+		case 'rúcula':
+		case 'couve':
+		case 'agrião':
+		case 'acelga':
+			verduras.push(Element);
+	}
+});
+
+console.log(`Frutas (${frutas.length}): ${showList(frutas)} `);
+console.log(`Legumes (${legumes.length}): ${showList(legumes)} `);
+console.log(`Verduras (${verduras.length}): ${showList(verduras)} `);
+
+//Funcao para imprimir os elementos da lista com um espaçamento
+function showList(list) {
+	let str = '';
+	for (let i = 0; i <= list.length; i++) {
+		str += `${list[i]}, `;
+	}
+	return str;
+}
---
+++ exercise02.js
@@ -0,0 +1,20 @@
+/*
+Temos a lista de empregados com seus respectivos nomes e salários.
+
+Utilize o método `some` para verificar se pelo menos um dos empregados tem salário maior ou igual a R$ 1.500,00.
+
+Utilize o método `some` novamente para verificar se pelo menos um dos empregados tem salário menor ou igual a R$ 1.000,00.
+
+Obs: Pode utilizar o `for` .
+
+*/
+
+console.log('---------- EMPREGADOS (SALARIO) ----------');
+
+const empregados = [
+	{ nome: 'João', salario: 1200 },
+	{ nome: 'Maria', salario: 1500 },
+	{ nome: 'Pedro', salario: 1800 },
+	{ nome: 'Ana', salario: 1400 },
+	{ nome: 'Carlos', salario: 2000 },
+];
